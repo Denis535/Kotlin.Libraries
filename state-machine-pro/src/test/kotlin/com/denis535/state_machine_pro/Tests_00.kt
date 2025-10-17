743ce4669c6b7f1ea30af03ada882a131104885c
Denis535 <dddenisss35@gmail.com>
2025-10-17 15:23:52 +0300

    Initial commit

---
+++ state-machine-pro/src/test/kotlin/com/denis535/state_machine_pro/Tests_00.kt
@@ -0,0 +1,13 @@
+package com.denis535.state_machine_pro
+
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
+
+public class Tests_00 {
+
+    @Test
+    fun Test_00() {
+        assertEquals(0, 0)
+    }
+
+}
