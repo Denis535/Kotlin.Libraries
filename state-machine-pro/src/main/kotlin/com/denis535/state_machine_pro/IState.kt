0b37a15ec51fb73b8032f2acfd7b564ea908005f
Denis535 <dddenisss35@gmail.com>
2025-10-17 20:46:01 +0300

    state-machine-pro

--- state-machine-pro/src/main/kotlin/com/denis535/state_machine_pro/IState.kt
+++ state-machine-pro/src/main/kotlin/com/denis535/state_machine_pro/IState.kt
@@ -1,4 +1,38 @@
 package com.denis535.state_machine_pro
 
-public interface IState {
+public interface IState<TMachineUserData, TStateUserData> {
+
+    public val IsDisposing: Boolean
+    public val IsDisposed: Boolean
+
+    public val Owner: Any?
+    public val Machine: IStateMachine<TMachineUserData, TStateUserData>?
+
+    public val IsRoot: Boolean
+    public val Root: IState<TMachineUserData, TStateUserData>
+
+    public val Parent: IState<TMachineUserData, TStateUserData>?
+    public val Ancestors: Sequence<IState<TMachineUserData, TStateUserData>>
+    public val AncestorsAndSelf: Sequence<IState<TMachineUserData, TStateUserData>>
+
+    public val Activity: Activity
+
+    public val Children: Sequence<IState<TMachineUserData, TStateUserData>>
+    public val Descendants: Sequence<IState<TMachineUserData, TStateUserData>>
+    public val DescendantsAndSelf: Sequence<IState<TMachineUserData, TStateUserData>>
+
+    public val UserData: TStateUserData
+
+    public fun Dispose()
+
+    public fun Attach(machine: IStateMachine<TMachineUserData, TStateUserData>, argument: Any?)
+    public fun Attach(parent: IState<TMachineUserData, TStateUserData>, argument: Any?)
+
+    public fun Detach(machine: IStateMachine<TMachineUserData, TStateUserData>, argument: Any?)
+    public fun Detach(parent: IState<TMachineUserData, TStateUserData>, argument: Any?)
+
+    public fun Activate(argument: Any?)
+
+    public fun Deactivate(argument: Any?)
+
 }
--- state-machine-pro/src/main/kotlin/com/denis535/state_machine_pro/IStateMachine.kt
+++ state-machine-pro/src/main/kotlin/com/denis535/state_machine_pro/IStateMachine.kt
@@ -1,4 +1,14 @@
 package com.denis535.state_machine_pro
 
-public interface IStateMachine {
+public interface IStateMachine<TMachineUserData, TStateUserData> {
+
+    public val IsDisposing: Boolean
+    public val IsDisposed: Boolean
+
+    public val Root: IState<TMachineUserData, TStateUserData>?
+
+    public val UserData: TMachineUserData
+
+    public fun Dispose()
+
 }
