041a7e2cf6a0dc7a91782916cc146f6070a471bd
Denis535 <dddenisss35@gmail.com>
2025-10-21 14:48:15 +0300

    game-framework-pro

---
+++ game-framework-pro/README.md
@@ -0,0 +1,48 @@
+# Overview
+
+The framework that allows you to design high-quality architecture of your game project.
+
+# Reference
+
+###### com.denis535.game_framework_pro
+
+- IDependencyProvider
+- DisposableBase
+
+###### com.denis535.game_framework_pro
+
+- ProgramBase
+- UI
+    - ThemeBase
+    - PlayListBase
+    - ScreenBase
+    - WidgetBase
+    - ViewableWidgetBase
+    - RouterBase
+- App
+    - ApplicationBase
+- Domain (Business, Game)
+    - GameBase
+    - PlayerBase
+    - EntityBase (Character, Thing, Vehicle, Environment)
+
+###### com.denis535.game_framework_pro
+
+- ProgramBase2
+- UI
+    - ThemeBase2
+    - PlayListBase2
+    - ScreenBase2
+    - WidgetBase2
+    - ViewableWidgetBase2
+    - RouterBase2
+- App
+    - ApplicationBase2
+- Domain
+    - GameBase2
+    - PlayerBase2
+
+# Links
+
+- https://github.com/Denis535/Kotlin.Libraries/tree/main/game-framework-pro
+- https://medium.com/@Denis535/gameframework-pro-6d00c75ff491
---
+++ game-framework-pro/build.gradle.kts
@@ -0,0 +1,86 @@
+val ProjectGroup = "io.github.denis535"
+val ProjectName = project.name
+val ProjectVersion = "1.0.0"
+val ProjectDescription = "The framework that allows you to design high-quality architecture of your game project."
+val ProjectUrl = "https://github.com/Denis535/Kotlin.Libraries/tree/main/game-framework-pro"
+
+plugins {
+    this.kotlin("jvm") version "2.2.20"
+    this.id("maven-publish")
+    this.id("signing")
+}
+
+group = ProjectGroup
+version = ProjectVersion
+
+repositories {
+    this.mavenCentral()
+}
+
+dependencies {
+    this.testImplementation(this.kotlin("test"))
+}
+
+java {
+    this.withSourcesJar()
+    this.withJavadocJar()
+}
+
+kotlin {
+    this.jvmToolchain(24)
+}
+
+tasks.test {
+    this.useJUnitPlatform()
+}
+
+publishing {
+    this.publications {
+        this.create<MavenPublication>("mavenJava") {
+            this.from(components["java"])
+            this.groupId = ProjectGroup
+            this.artifactId = ProjectName
+            this.version = ProjectVersion
+            this.pom {
+                this.name.set(ProjectName)
+                this.description.set(ProjectDescription)
+                this.url.set(ProjectUrl)
+                this.licenses {
+                    this.license {
+                        this.name.set("MIT License")
+                        this.url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                this.developers {
+                    this.developer {
+                        this.id.set("denis535")
+                        this.name.set("Denis535")
+                    }
+                }
+                this.scm {
+                    this.connection.set("scm:git:git://https://github.com/Denis535/Kotlin.Libraries.git")
+                    this.developerConnection.set("scm:git:ssh://https://github.com/Denis535/Kotlin.Libraries.git")
+                    this.url.set("https://github.com/Denis535/Kotlin.Libraries/tree/main/game-framework-pro")
+                }
+            }
+        }
+    }
+//    this.repositories {
+//        this.maven {
+//            this.name = "sonatype"
+//            this.url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+//            this.credentials {
+//                this.username = project.findProperty("ossrhUsername") as String?
+//                this.password = project.findProperty("ossrhPassword") as String?
+//            }
+//        }
+//    }
+}
+
+signing {
+    this.useInMemoryPgpKeys(
+        File("${projectDir}/0x32672C2E-sec.asc").readText(),
+        "qwerty"
+    )
+    this.sign(publishing.publications["mavenJava"])
+}
