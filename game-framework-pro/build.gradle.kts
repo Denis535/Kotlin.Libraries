d4b56f403dcaf8b3f0c62516f518d10ca0ad563d
Denis535 <dddenisss35@gmail.com>
2025-10-22 12:29:39 +0300

    game-framework-pro

--- game-framework-pro/build.gradle.kts
+++ game-framework-pro/build.gradle.kts
@@ -1,3 +1,21 @@
 plugins {
     this.id("java-platform")
+    this.id("maven-publish")
+    this.id("signing")
+}
+
+subprojects {
+    this.group = project.findProperty("project.group") as String
+    this.version = project.findProperty("project.version") as String
+    this.repositories {
+        this.mavenCentral()
+        this.maven {
+            this.name = "GitHubPackages"
+            this.url = uri("https://maven.pkg.github.com/Denis535/Kotlin.Libraries/")
+            this.credentials {
+                this.username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR") ?: error("GITHUB_ACTOR is not found")
+                this.password = project.findProperty("gpr.key") as String? ?: System.getenv("GITHUB_TOKEN") ?: error("GITHUB_TOKEN is not found")
+            }
+        }
+    }
 }
--- game-framework-pro/game-framework-pro/build.gradle.kts
+++ game-framework-pro/game-framework-pro/build.gradle.kts
@@ -4,15 +4,10 @@ plugins {
     this.id("signing")
 }
 
-group = findProperty("project.group") as String
-version = findProperty("project.version") as String
-
 dependencies {
-    this.testImplementation(kotlin("test"))
-}
-
-repositories {
-    this.mavenCentral()
+    this.implementation("io.github.denis535:state-machine-pro:1.0.0")
+    this.implementation("io.github.denis535:tree-machine-pro:1.0.0")
+    this.testImplementation(this.kotlin("test"))
 }
 
 java {
@@ -28,42 +23,42 @@ tasks.test {
     this.useJUnitPlatform()
 }
 
-publishing {
-    this.publications {
-        this.create<MavenPublication>("mavenJava") {
-            this.from(components["java"])
-            this.groupId = findProperty("project.group") as String
-            this.artifactId = findProperty("project.name") as String
-            this.version = findProperty("project.version") as String
-            this.pom {
-                this.name = findProperty("project.name") as String
-                this.description = findProperty("project.description") as String
-                this.url = findProperty("project.url") as String
-                this.licenses {
-                    this.license {
-                        this.name = "MIT License"
-                        this.url = "https://opensource.org/licenses/MIT"
-                    }
-                }
-                this.developers {
-                    this.developer {
-                        this.id = "denis535"
-                        this.name = "Denis535"
-                    }
-                }
-                this.scm {
-                    this.connection = "scm:git:git://https://github.com/Denis535/Kotlin.Libraries.git"
-                    this.developerConnection = "scm:git:ssh://https://github.com/Denis535/Kotlin.Libraries.git"
-                    this.url = findProperty("project.url") as String
-                }
-            }
-        }
-    }
-}
+//publishing {
+//    this.publications {
+//        this.create<MavenPublication>("mavenJava") {
+//            this.from(components["java"])
+//            this.groupId = findProperty("project.group") as String
+//            this.artifactId = findProperty("project.name") as String
+//            this.version = findProperty("project.version") as String
+//            this.pom {
+//                this.name = findProperty("project.name") as String
+//                this.description = findProperty("project.description") as String
+//                this.url = findProperty("project.url") as String
+//                this.licenses {
+//                    this.license {
+//                        this.name = "MIT License"
+//                        this.url = "https://opensource.org/licenses/MIT"
+//                    }
+//                }
+//                this.developers {
+//                    this.developer {
+//                        this.id = "denis535"
+//                        this.name = "Denis535"
+//                    }
+//                }
+//                this.scm {
+//                    this.connection = "scm:git:git://https://github.com/Denis535/Kotlin.Libraries.git"
+//                    this.developerConnection = "scm:git:ssh://https://github.com/Denis535/Kotlin.Libraries.git"
+//                    this.url = "https://github.com/Denis535/Kotlin.Libraries"
+//                }
+//            }
+//        }
+//    }
+//}
 
-signing {
-    this.useInMemoryPgpKeys(
-        File("${projectDir}/../0x32672C2E-sec.asc").readText(), "qwerty"
-    )
-    this.sign(publishing.publications["mavenJava"])
-}
+//signing {
+//    this.useInMemoryPgpKeys(
+//        File("${projectDir}/../0x32672C2E-sec.asc").readText(), "qwerty"
+//    )
+//    this.sign(publishing.publications["mavenJava"])
+//}
