3c06227be2506f7c4d7b63ea47d2375918a989e5
Denis535 <dddenisss35@gmail.com>
2025-10-23 10:53:28 +0300

    game-framework-pro

--- game-framework-pro/game-framework-pro/src/main/kotlin/com/denis535/game_framework_pro/AbstractScreen.kt
+++ game-framework-pro/game-framework-pro/src/main/kotlin/com/denis535/game_framework_pro/AbstractScreen.kt
@@ -4,9 +4,18 @@ import com.denis535.tree_machine_pro.*
 
 public abstract class AbstractScreen : AbstractCloseable {
 
-    public constructor()
+    protected val Machine: TreeMachine<AbstractScreen, AbstractWidget>
+        get() {
+            assert(!this.IsClosed)
+            return field
+        }
+
+    public constructor() {
+        this.Machine = TreeMachine<AbstractScreen, AbstractWidget>(this)
+    }
 
     public override fun OnClose() {
+        this.Machine.close()
     }
 
 }
--- game-framework-pro/game-framework-pro/src/main/kotlin/com/denis535/game_framework_pro/AbstractTheme.kt
+++ game-framework-pro/game-framework-pro/src/main/kotlin/com/denis535/game_framework_pro/AbstractTheme.kt
@@ -4,9 +4,60 @@ import com.denis535.state_machine_pro.*
 
 public abstract class AbstractTheme : AbstractCloseable {
 
-    public constructor()
+    protected val Machine: StateMachine<AbstractTheme, AbstractPlayList>
+        get() {
+            assert(!this.IsClosed)
+            return field
+        }
+
+    public constructor() {
+        this.Machine = StateMachine<AbstractTheme, AbstractPlayList>(this)
+    }
 
     public override fun OnClose() {
+        this.Machine.close()
     }
+
+}
+
+public abstract class AbstractPlayList {
+
+    public val IsClosing: Boolean
+        get() {
+            return this.StateMutable.IsClosing
+        }
+    public val IsClosed: Boolean
+        get() {
+            return this.StateMutable.IsClosed
+        }
+
+    protected val Theme: AbstractTheme?
+        get() {
+            assert(!this.IsClosed)
+            return this.StateMutable.Machine?.UserData
+        }
+    public val State: AbstractState<AbstractTheme, AbstractPlayList>
+        get() {
+            assert(!this.IsClosed)
+            return this.StateMutable
+        }
+    protected val StateMutable: State<AbstractTheme, AbstractPlayList>
+        get() {
+            assert(!this.IsClosed)
+            return field
+        }
+
+    public constructor() {
+        this.StateMutable = State<AbstractTheme, AbstractPlayList>(this).apply {
+            this.OnCloseCallback = this@AbstractPlayList::OnClose
+            this.OnActivateCallback = this@AbstractPlayList::OnActivate
+            this.OnDeactivateCallback = this@AbstractPlayList::OnDeactivate
+        }
+    }
+
+    protected abstract fun OnClose()
+
+    protected abstract fun OnActivate(argument: Any?)
+    protected abstract fun OnDeactivate(argument: Any?)
 
 }
