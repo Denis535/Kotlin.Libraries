fbece02bb1ef43c474192d15ff54ea2e1c75cc4c
Denis535 <dddenisss35@gmail.com>
2025-10-24 14:21:50 +0300

    game-framework-pro

--- game-framework-pro/core/build.gradle.kts
+++ game-framework-pro/core/build.gradle.kts
@@ -22,42 +22,35 @@ tasks.test {
     this.useJUnitPlatform()
 }
 
-//publishing {
-//    this.publications {
-//        this.create<MavenPublication>("mavenJava") {
-//            this.from(components["java"])
-//            this.groupId = findProperty("project.group") as String
-//            this.artifactId = findProperty("project.name") as String
-//            this.version = findProperty("project.version") as String
-//            this.pom {
-//                this.name = findProperty("project.name") as String
-//                this.description = findProperty("project.description") as String
-//                this.url = findProperty("project.url") as String
-//                this.licenses {
-//                    this.license {
-//                        this.name = "MIT License"
-//                        this.url = "https://opensource.org/licenses/MIT"
-//                    }
-//                }
-//                this.developers {
-//                    this.developer {
-//                        this.id = "denis535"
-//                        this.name = "Denis535"
-//                    }
-//                }
-//                this.scm {
-//                    this.connection = "scm:git:git://https://github.com/Denis535/Kotlin.Libraries.git"
-//                    this.developerConnection = "scm:git:ssh://https://github.com/Denis535/Kotlin.Libraries.git"
-//                    this.url = "https://github.com/Denis535/Kotlin.Libraries"
-//                }
-//            }
-//        }
-//    }
-//}
-
-//signing {
-//    this.useInMemoryPgpKeys(
-//        File("${projectDir}/../0x32672C2E-sec.asc").readText(), "qwerty"
-//    )
-//    this.sign(publishing.publications["mavenJava"])
-//}
+publishing {
+    this.publications {
+        this.create<MavenPublication>("mavenJava") {
+            this.from(components["java"])
+            this.groupId = project.findProperty("project.group") as String
+            this.artifactId = project.findProperty("project.name") as String
+            this.version = project.findProperty("project.version") as String
+            this.pom {
+                this.name = project.findProperty("project.name") as String
+                this.description = project.findProperty("project.description") as String
+                this.url = project.findProperty("project.url") as String
+                this.licenses {
+                    this.license {
+                        this.name = "MIT License"
+                        this.url = "https://opensource.org/licenses/MIT"
+                    }
+                }
+                this.developers {
+                    this.developer {
+                        this.id = "denis535"
+                        this.name = "Denis535"
+                    }
+                }
+                this.scm {
+                    this.connection = "scm:git:git://https://github.com/Denis535/Kotlin.Libraries.git"
+                    this.developerConnection = "scm:git:ssh://https://github.com/Denis535/Kotlin.Libraries.git"
+                    this.url = "https://github.com/Denis535/Kotlin.Libraries"
+                }
+            }
+        }
+    }
+}
