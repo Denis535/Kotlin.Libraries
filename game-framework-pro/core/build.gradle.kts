35645230fdc51c9ff6ece66b72eab2e9f7286232
Denis535 <dddenisss35@gmail.com>
2025-10-24 20:41:35 +0300

    game-framework-pro

--- game-framework-pro/core/build.gradle.kts
+++ game-framework-pro/core/build.gradle.kts
@@ -53,7 +53,7 @@ publishing {
             }
         }
     }
-    this@publishing.repositories {
+    this.repositories {
         this.maven {
             this.name = "GitHubPackages"
             this.url = uri("https://maven.pkg.github.com/Denis535/Kotlin.Libraries")
