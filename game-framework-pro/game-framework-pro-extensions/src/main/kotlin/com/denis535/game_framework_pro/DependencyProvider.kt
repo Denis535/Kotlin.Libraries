9726101ed8535f888810483502f815ede1a9775f
Denis535 <dddenisss35@gmail.com>
2025-10-24 13:48:27 +0300

    game-framework-pro

--- game-framework-pro/game-framework-pro-extensions/src/main/kotlin/com/denis535/game_framework_pro/DependencyProvider.kt
+++ game-framework-pro/game-framework-pro-extensions/src/main/kotlin/com/denis535/game_framework_pro/DependencyProvider.kt
@@ -8,18 +8,17 @@ public interface DependencyProvider {
 
 }
 
-//inline fun <reified T : Any> DependencyProvider.RequireDependency(argument: Any? = null): T {
-//    return this.GetDependency(T::class, argument) as T? ?: error("Dependency ${T::class} (${argument ?: "Null"}) was not found")
-//}
-//
 //inline fun <reified T : Any> DependencyProvider.GetDependency(argument: Any? = null): T? {
 //    return this.GetDependency(T::class, argument) as T?
 //}
-
-fun <T : Any> DependencyProvider.RequireDependency(clazz: KClass<*>, argument: Any? = null): T {
-    return this.GetDependency(clazz, argument) as T? ?: error("Dependency $clazz (${argument ?: "Null"}) was not found")
-}
+//inline fun <reified T : Any> DependencyProvider.RequireDependency(argument: Any? = null): T {
+//    return this.GetDependency(T::class, argument) as T? ?: error("Dependency ${T::class} (${argument ?: "Null"}) was not found")
+//}
 
 fun <T : Any> DependencyProvider.GetDependency(clazz: KClass<*>, argument: Any? = null): T? {
     return this.GetDependency(clazz, argument) as T?
+}
+
+fun <T : Any> DependencyProvider.RequireDependency(clazz: KClass<*>, argument: Any? = null): T {
+    return this.GetDependency(clazz, argument) as T? ?: error("Dependency $clazz (${argument ?: "Null"}) was not found")
 }
