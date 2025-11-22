33396f3fcf2252cb941e416de1350644c571b3bd
Denis535 <dddenisss35@gmail.com>
2025-11-22 18:56:51 +0200

    build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,5 +2,10 @@ plugins {
     this.id("org.jetbrains.kotlin.jvm") version "2.2.21"
     this.id("maven-publish")
 }
+
 group = "com.github.Denis535.Kotlin.Libraries"
 version = "v1.0.0"
+
+repositories {
+    this.mavenCentral()
+}
