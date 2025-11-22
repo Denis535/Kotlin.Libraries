5f73a3a11949859d54d550a73b66b00ec38db695
Denis535 <dddenisss35@gmail.com>
2025-11-22 19:27:13 +0200

    tree-machine-pro

--- tree-machine-pro/settings.gradle.kts
+++ tree-machine-pro/settings.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    this.id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    this.id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "tree-machine-pro"
