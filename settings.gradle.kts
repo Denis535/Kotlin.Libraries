f8fe4a3e5001f0370f11ba12755a9af69d39c83c
Denis535 <dddenisss35@gmail.com>
2025-11-23 08:51:14 +0200

    settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,4 @@
-plugins {
-    this.id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
-
 rootProject.name = "Kotlin.Libraries"
-
-include("state-machine-pro", "tree-machine-pro", "game-framework-pro")
+include("state-machine-pro")
+include("tree-machine-pro")
+include("game-framework-pro")
